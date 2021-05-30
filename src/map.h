daebb54905753014517de9d2e5c0a783b14df8fc
David <davidcirotaborda@gmail.com>
2021-05-30 18:18:55 -0300

    prototype updates for using memory-safe types of the gsl library in projected methods

--- src/map.h
+++ src/map.h
@@ -1,21 +1,44 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <gsl/gsl_matrix.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct map* map_h;
 
-map_h map_create();
-void map_destroy(map_h m);
+map_h // allocate map and set user parameters
+map_create(int (*fw)(const gsl_vector* x0,
+                     gsl_vector*       x1,
+                     void*             params), // forward map
+           int (*bw)(const gsl_vector* x0,
+                     gsl_vector*       x1,
+                     void*             params), // backwards map
+           void* params             // map parameters
+);
 
-void map_set_forward(map_h m, int(*fw)(double* x0, double* x1, void* params));
-void map_set_backward(map_h m, int(*bw)(double* x0, double* x1, void* params));
-void map_set_params(map_h m, void* params);
+void // free map memory
+map_destroy(map_h m);
 
-void map_fw(map_h m, double* x0, double* x1);
-void map_bw(map_h m, double* x0, double* x1);
+void // define jacobian function for the forward map
+map_set_jacobian(map_h m,
+                 int (*jac)(const gsl_vector* x0,
+                            gsl_matrix*       J,
+                            void*             params));
 
-void map_fixed_point(map_h m, size_t order, double* x0, double* x_fixed);
+int // map forward 'order' times
+map_fw(map_h m, size_t order, const gsl_vector* x0, gsl_vector* x1);
+
+int // map backwards 'order' times
+map_bw(map_h m, size_t order, const gsl_vector* x0, gsl_vector* x1);
+
+int // evaluate map jacobian of 'order'
+map_jac(map_h m, size_t order, const gsl_vector* x0, gsl_matrix* J);
+
+int // search fixed point of 'order'
+map_fixed_point(map_h             m,
+                size_t            order,
+                const gsl_vector* x0,
+                gsl_vector*       x_fixed);
 
 #endif // MAP_H
